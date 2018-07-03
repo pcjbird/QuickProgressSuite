3eef6a8f0cbac454171e0b67ded744918ffd954e
pcjbird <pcjbird@hotmail.com>
2018-07-03 17:35:19 +0800

    some code

--- QuickProgressSuite/Circular/QuickProgressViewCircle.h
+++ QuickProgressSuite/Circular/QuickProgressViewCircle.h
@@ -14,4 +14,55 @@
 IB_DESIGNABLE
 @interface QuickProgressViewCircle : QuickProgressView
 
+/**
+ * Progress bar rotation (Clockewise)    [0,100]
+ */
+@property (nonatomic,assign) IBInspectable CGFloat   progressRotationAngle;
+
+/**
+ * Set a partial angle for the progress bar    [0,100]
+ */
+@property (nonatomic,assign) IBInspectable CGFloat   progressAngle;
+
+/**
+ * @brief 进度条宽度 The width of the progress bar (user space units)    [0,∞)
+ */
+@property (nonatomic,assign) IBInspectable CGFloat   progressLineWidth;
+
+/**
+ * @brief 进度条前景色 The color of the progress bar
+ */
+@property (nonatomic,strong) IBInspectable UIColor   *progressColor;
+
+/**
+ * @brief 进度条描边着色 The color of the progress bar frame
+ */
+@property (nonatomic,strong) IBInspectable UIColor   *progressStrokeColor;
+
+/**
+ * @brief 进度条定点类型 The shape of the progress bar cap    {kCGLineCapButt=0, kCGLineCapRound=1, kCGLineCapSquare=2}
+ */
+@property (nonatomic,assign) IBInspectable NSInteger progressCapType;
+
+/**
+ * The width of the background bar (user space units)    [0,∞)
+ */
+@property (nonatomic,assign) IBInspectable CGFloat   emptyLineWidth;
+
+/**
+ * The color of the background bar
+ */
+@property (nonatomic,strong) IBInspectable UIColor   *emptyLineColor;
+
+/**
+ * The color of the background bar stroke color
+ */
+@property (nonatomic,strong) IBInspectable UIColor   *emptyLineStrokeColor;
+
+/**
+ * The shape of the background bar cap    {kCGLineCapButt=0, kCGLineCapRound=1, kCGLineCapSquare=2}
+ */
+@property (nonatomic,assign) IBInspectable NSInteger emptyCapType;
+
+
 @end
--- QuickProgressSuite/Circular/QuickProgressViewCircle.m
+++ QuickProgressSuite/Circular/QuickProgressViewCircle.m
@@ -8,14 +8,104 @@
 
 #import "QuickProgressViewCircle.h"
 
+@interface QuickProgressViewCircle()
+
+@property(nonatomic, strong) CAShapeLayer *emptyLineLayer;
+@property(nonatomic, strong) CAShapeLayer *progressLayer;
+
+@end
+
 @implementation QuickProgressViewCircle
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+-(instancetype)initWithFrame:(CGRect)frame
+{
+    if(self = [super initWithFrame:frame])
+    {
+        
+    }
+    return self;
 }
-*/
+
+-(instancetype)initWithCoder:(NSCoder *)aDecoder
+{
+    if(self = [super initWithCoder:aDecoder])
+    {
+        
+    }
+    return self;
+}
+
+-(CAShapeLayer*)emptyLineLayer
+{
+    if(![_emptyLineLayer isKindOfClass:[CAShapeLayer class]])
+    {
+        _emptyLineLayer = [CAShapeLayer layer];
+    }
+    return _emptyLineLayer;
+}
+
+-(CAShapeLayer*)progressLayer
+{
+    if(![_progressLayer isKindOfClass:[CAShapeLayer class]])
+    {
+        _progressLayer = [CAShapeLayer layer];
+    }
+    return _progressLayer;
+}
+
+-(UIBezierPath *) createEmptyLinePath
+{
+    
+    CGRect rect = self.bounds;
+    CGPoint center = {CGRectGetMidX(rect), CGRectGetMidY(rect)};
+    CGFloat radius = MIN(CGRectGetWidth(rect), CGRectGetHeight(rect))/2 - MAX(self.emptyLineWidth, self.progressLineWidth)/2.f;
+    
+    CGMutablePathRef arc = CGPathCreateMutable();
+    CGPathAddArc(arc, NULL,
+                 center.x, center.y, radius,
+                 (self.progressAngle/100.f)*M_PI-((-self.progressRotationAngle/100.f)*2.f+0.5)*M_PI,
+                 //-(self.progressAngle/100.f)*M_PI-((-self.progressRotationAngle/100.f)*2.f+0.5)*M_PI,
+                 (self.progressAngle/100.f)*M_PI-((-self.progressRotationAngle/100.f)*2.f+0.5)*M_PI -(2.f*M_PI)*(self.progressAngle/100.f)*(100.f-100.f*self.progress)/100.f /*- (2.f*M_PI)*(self.progressAngle/100.f)**/,
+                 YES);
+    
+    
+    CGPathRef strokedArc =
+    CGPathCreateCopyByStrokingPath(arc, NULL,
+                                   self.emptyLineWidth,
+                                   (CGLineCap)self.emptyCapType,
+                                   kCGLineJoinMiter,
+                                   10);
+    UIBezierPath * path = [UIBezierPath bezierPathWithCGPath:strokedArc];
+    CGPathRelease(arc);
+    CGPathRelease(strokedArc);
+    
+    return path;
+}
+
+-(UIBezierPath *) createProgressPath
+{
+    CGRect rect = self.bounds;
+    CGPoint center = {CGRectGetMidX(rect), CGRectGetMidY(rect)};
+    CGFloat radius = MIN(CGRectGetWidth(rect), CGRectGetHeight(rect))/2 - MAX(self.emptyLineWidth, self.progressLineWidth)/2.f;
+    
+    CGMutablePathRef arc = CGPathCreateMutable();
+    CGPathAddArc(arc, NULL,
+                 center.x, center.y, radius,
+                 (self.progressAngle/100.f)*M_PI-((-self.progressRotationAngle/100.f)*2.f+0.5)*M_PI -(2.f*M_PI)*(self.progressAngle/100.f)*(100.f-100.f*self.progress)/100.f ,
+                 -(self.progressAngle/100.f)*M_PI-((-self.progressRotationAngle/100.f)*2.f+0.5)*M_PI,
+                 YES);
+    
+    CGPathRef strokedArc =
+    CGPathCreateCopyByStrokingPath(arc, NULL,
+                                   self.progressLineWidth,
+                                   (CGLineCap)self.progressCapType,
+                                   kCGLineJoinMiter,
+                                   10);
+    UIBezierPath * path = [UIBezierPath bezierPathWithCGPath:strokedArc];
+    CGPathRelease(arc);
+    CGPathRelease(strokedArc);
+    
+    return path;
+}
 
 @end
